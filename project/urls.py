7b29f1c94fc377da074735ce2def0687ed1692eb
Miguel Angel Cumpa Ascuña <themiseck.rock@gmail.com>
2011-12-24 02:36:25 -0500

    Update N° 2

---
+++ project/urls.py
@@ -0,0 +1,32 @@
+
+from django.conf.urls.defaults import *
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+    # grapelli admin urls
+    (r'^grappelli/', include('grappelli.urls')),
+)
+
+if settings.DEBUG:
+    from django.views.static import serve
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$',
+            serve, {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        url(r'^static/(?P<path>.*)$',
+            serve, {'document_root': settings.STATIC_ROOT}),
+        url(r'^favicon\.ico$', 
+            'django.views.generic.simple.redirect_to', 
+            {'url': '/static/images/favicon.ico'}),
+    )
